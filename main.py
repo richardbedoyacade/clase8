83d01c56f2ad93380f407f1c9c341ad84fc45bec
richard bedoya <richardbedo16@gmail.com>
2023-05-16 08:30:22 -0500

    crea varias listas y muestra su tamaño, crea el .gitignore

---
+++ main.py
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+edad = 12
+
+altura = 1.79
+
+nombre = "juan"
+
+nombre = True
+
+lista1 = [10, 5, 3, 9]
+
+lista2 = [1.78, 2.66, 1.55, 89.4,6.9]
+
+lista3 = ["lunes","martes","miercoles"]
+
+lista4 = ["juan",45,1.92,False]
+
+
+
+if __name__ == '__main__':
+
+    print(len(lista1))
+    print(len(lista2))
+    print(len(lista3))
+    print(len(lista4))
+
+    print()
+    print(lista1)
+    lista1[0] = 1
+    print()
+    print(lista1)
+
+    print(lista1[3])
